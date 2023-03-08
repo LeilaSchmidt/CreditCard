c851f9bc8c3312d19d4ea3c0b00c72b4ca076d6a
Leila Schmidt <lnsnani80@gmail.com>
2023-03-09 00:02:03 +0100

    Add files via upload

---
+++ CreditCard/CreditCard/Card.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreditCard
+{
+    class Card
+    {
+        public void cardChoice()
+        {
+            Console.WriteLine("Welcome, which of the three credit cards listed below would you like to apply for?");
+            Console.WriteLine("VISA, Mastercard, American Express");
+        }
+        public void GetCardDetails()
+        {
+            Console.WriteLine("Please e");
+        }
+    }
+    class Visa : Card
+    {
+        public int cardNum = 1;
+    }
+
+    class Mastercard : Card
+    {
+        public int cardNum = 2;
+    }
+
+    class AmericanExpress : Card
+    {
+        public int cardNum = 3;
+    }
+
+}
---
+++ CreditCard/CreditCard/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace CreditCard
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Card card = new Card();
+            
+        }
+    }
+}
